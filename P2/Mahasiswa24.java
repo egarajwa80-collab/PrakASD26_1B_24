77de8548f6bc34fcb641da2000ae9e4af089648b
254107020205 <254107020205@student.polinema.ac.id>
2026-03-01 17:23:22 +0700

    P2_percobaan_1_24

---
+++ P2/Mahasiswa24.java
@@ -0,0 +1,40 @@
+package P2;
+
+import java.util.Scanner;
+
+public class Mahasiswa24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String nama;
+        String nim;
+        String kelas;
+        double ipk;
+
+        void tampilkanInformasi () {
+            System.out.println("Nama: " + nama);
+            System.out.println("NIM: " + nim);
+            System.out.println("IPK: " + ipk);
+            System.out.println("Kelas: " + kelas);
+        }
+        void ubahKelas (String kelasBaru) {
+            kelas = kelasBaru;
+        }
+        Void updateIPK(double ipkBaru) {
+            ipk = ipkBaru;
+        }
+
+        String nilaiKinerja() {
+            if (ipk >= 3.5) {
+                return "Kinerja Sangat Baik";
+            } else if (ipk >= 3.0) {
+                return "Kinerja Baik";
+            } else if (ipk >= 2.5) {
+                return "Kineja Cukup";
+            } else {
+                return "Kinerja Kurang";
+            }
+        }
+
+    }
+}
