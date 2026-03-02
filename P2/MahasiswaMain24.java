2af4ad9b707db201638da51f1014e7a3e1a228a4
254107020205 <254107020205@student.polinema.ac.id>
2026-03-02 17:02:22 +0700

    P2_percobaan6_24

--- P2/MahasiswaMain24.java
+++ P2/MahasiswaMain24.java
@@ -11,15 +11,18 @@ public class MahasiswaMain24 {
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
-
         mhs1.tampilkanInformasi();
 
         Mahasiswa24 mhs2 = new Mahasiswa24 ("Annisa Nabila","2141720160",3.25,"TI 2L");
         mhs2.updateIPK(3.30);
         mhs2.tampilkanInformasi();
+        
+        Mahasiswa24 mhsEga= new Mahasiswa24("Budi Santoso", "2241720188", 2.75, "TI 1B");
+        mhsEga.updateIPK(3.00);
+        mhsEga.tampilkanInformasi();
+
 
         System.out.println("Nilai Kinerja: " + mhs1.nilaiKinerja());
     }
