3664c3f0c2f0b912e702faec84dfe6db1226ad19
254107020205 <254107020205@student.polinema.ac.id>
2026-03-04 00:17:03 +0700

    P3_percobaan6_24

---
+++ P3/Praktikum03/Matakuliahdemo24.java
@@ -0,0 +1,38 @@
+package P3.Praktikum03;
+import java.util.Scanner;
+public class Matakuliahdemo24 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        MataKuliah24[] arrayOfMataKuliah = new MataKuliah24[3];
+        String kode, nama, dummy;
+        int sks, jumlahjam;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Mata Kuliah ke-" + (i + 1));
+            System.out.print("Kode   : ");
+            kode = sc.nextLine();
+            System.out.print("Nama   : ");
+            nama = sc.nextLine();
+            System.out.print("SKS    : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahjam = Integer.parseInt(dummy);
+            System.out.println("---------------------------------");
+
+     
+            arrayOfMataKuliah[i] = new MataKuliah24(kode, nama, sks, jumlahjam);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Mata Kuliah ke-" + (i + 1));
+            System.out.println("Kode   : " + arrayOfMataKuliah[i].getKode());
+            System.out.println("Nama   : " + arrayOfMataKuliah[i].getNama());
+            System.out.println("SKS    : " + arrayOfMataKuliah[i].getSks());
+            System.out.println("Jumlah Jam : " + arrayOfMataKuliah[i].getJumlahjam());
+            System.out.println("---------------------------------");
+        }
+        sc.close();
+    }
+}
